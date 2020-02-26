494a7ff5f1b404537378a1ff8ae8b898abb2ee03
Reny Ramos <renyramosgarcia@gmail.com>
2020-02-25 23:47:12 -0400

    Rutas nuevas

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ route::get('/success/{msg?}/{title?}', 'MsgController@success')->name('alert.suc
 
 route::get('/message/create', 'MsgController@create')->name('msg.create');
 route::post('/message', 'MsgController@post')->name('msg.post');
+
+route::get('/table-cancel', 'TableCancelController@index')->name('table.cancel.index');
+route::delete('/table-cancel/{id}', 'TableCancelController@destroy')->name('table.cancel.destroy');
