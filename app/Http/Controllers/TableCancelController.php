3c8ba42194cb3be512d5670d48321bc4b44a2417
Reny Ramos <renyramosgarcia@gmail.com>
2020-02-25 23:45:48 -0400

    Controller para primer utilidad de tablas.

---
+++ app/Http/Controllers/TableCancelController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TableCancelController extends Controller
+{
+    public function index()
+    {
+        return view('table.cancel.index');
+    }
+
+    public function destroy()
+    {
+        return view('table.cancel.index');
+    }
+}
