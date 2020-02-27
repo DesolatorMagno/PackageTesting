5d3ffc8ee48469fbf214c9873db0c7ad8f9ad33a
Reny Ramos <renyramosgarcia@gmail.com>
2020-02-27 00:15:49 -0400

    Link in welcome for del-table

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,14 +87,12 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
 
-                    <a href="{{ route('table.cancel.index') }}">Tablas</a>
+                    <a href="{{ route('table.del.index') }}">Tabla Del</a>
+                    <a href="{{ route('table.cancel.index') }}">Tabla Cancel</a>
                     <a href="{{ route('msg.create') }}">Mensajes</a>
                 </div>
             </div>
